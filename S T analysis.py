3855399b065530a2293d878f13f7f65adbcf00cc
andjroo <123885129+andjroo@users.noreply.github.com>
2023-01-29 15:56:17 +0000

    Update and rename code.py to S T analysis.py

---
+++ S T analysis.py
@@ -0,0 +1,23 @@
+# import required modules and packages
+from Bio import SeqIO
+
+# read protein sequence
+#seq = [str(record.seq) for record in SeqIO.parse("sequence.fasta", "fasta")][0]
+sequence = input("P61764.fasta", "fasta")
+seq = [str(record.seq) for record in SeqIO.parse(sequence, "fasta")][0]
+# length of protein sequence
+N = len(seq)
+print(f"Length of Sequence: {N}\n{seq}")
+
+# Count Serine (S)
+s = seq.count("S")
+# calculate percentage of S
+s_pcnt = round((s/N)*100, 2)
+
+# Count Threonine (T)
+t = seq.count("T")
+# calculate percentage of T
+t_pcnt = round((t/N)*100, 2)
+
+print(f"Percentage of Serine (S): {s_pcnt}%")
+print(f"Percentage of Threonine (T): {t_pcnt}%")
